9e87c31f81a29ff092d8281e96b815b4f063f6a5
Mark2tu <33594111+Mark2tu@users.noreply.github.com>
2024-07-06 19:43:27 +0800

    Update OmoFun.js

--- js/OmoFun.js
+++ js/OmoFun.js
@@ -1,60 +1,19 @@
+muban.mxpro.二级.desc = '.module-info-item:eq(3)&&Text;;;.module-info-item-content:eq(1)&&Text;.module-info-item-content:eq(0)&&Text';
+muban.mxpro.二级.tabs = '#y-playList&&.tab-item';
 var rule = {
-    title: 'OmoFun[漫]',
-    host: 'http://omofun1.xyz',
-    hostJs: $js.toString(() => {
-        print(HOST);
-        let html = request(HOST, {headers: {"User-Agent": PC_UA}});
-        let src = jsp.pdfh(html, ".content-top&&ul&&a&&href");
-        print(src);
-        HOST = src;
-    }),
-    url: "/vod/showfyfilter/page/fypage.html",
-    // searchUrl: "/vod/search/page/fypage/wd/**.html",
-    searchable: 2,
-    quickSearch: 0,
-    filterable: 1,
-    filter: "H4sIAAAAAAAAA+2a2U4jRxSG38XXRG6zDnM3+77vE82FM7ESFEIkIJHQCAkwNjabATF4CGbLAAaCwSwhYMfwMq5q+y3S7iqfqnOMpo0go9GkL/v7f2o5XdX+q+l3Hp/n4rfvPD8FujwXPeZ2ls0NeWo8bf6fA/r1b/7WXwO2sc3CLLRaDK6WsHXh83TXSDy4ms8lzOiAVBqVMpVg0aRSmkAxI9s8GFLKBaUkx9lhVinNoPDeMd4zpRSfoTqKJlFzPjU6HvmQz0Q1qdbT/aYkiqmLv1RTh+tPTJ3MT9K3rf6ODq9EeJ7IIRGeFXJIhOuLexEI1wz3IhAuEG5DIFwnPA6Byo5CaoUNryOHRDCOwS0zhx0SaXMxJ7N0LiUEjuUBOheJYKSplfzRAh6pQNBGeKI4vYbbEAjamFu3ZofbEKj6+8L7NsypcewQCBzBQd73O3YIBBXLxljoAFdMoLKjODvBPywjh0TQy9RAIZrBvQgE9TjaNCf/YrltXBKg4IstFT6SVSIQOEbDLLaDHQLBKjkes+4oXiUCqbuT4LPj5O7YCBz9x+afeM4SQd1y42Y2ccKkkIA2eiLNhjPaRi9fV7XRl5LF6bCkXn97wO8VyNvyvbpTK9P8YAuZBNJNLJbmh0e4JRvpJvNolM3kkEkg3cR331OTQLqpMLRNTQKhluLLPLGBW7IRmt38Gm1JIDS7o7+pSSA88HTlwNO0pZE0y6zglmyEWuqPWbeBRdZwY2WKCrF8bMZSZnQa16JMUdeDC3zo2GoC916myBraz2ensM9GtklbfYWtVGG1R60+uK5q9c3kLH+5l1Z/2w9eibTbTB0SwZLaWaIOieAGx3NsJE5NimoLocIkkLaeqEMibe1WOATSVlLFnAXSas82g9ghENr2h3v5bE7b9uXragpfa/gulDvoCvjbvTbQ1CaqNulqI1UbdbWBqg26Wk/Vel2to2qdrtZStVZXfVT16apBVQOXMxXnWyNaOcvXpJyA9XJegpsV6OwMtHsvgXKZKJdBuUKUK6BcJcpVUK4R5Roo14lyHZQbRLkByk2i3ATlFlFugXKbKLdBuUOUO6DcJcpdUO4R5R4o94lyH5QHRHkAykOiPATlEVEegfKYKI9BeUKUJ6A8JcpTUJ4R5Rkoz4nyHJQXRHkBykuivATlFVFegfKaKK9BMb5pJlqJ6FuAj06wTExtAbjGW4DH94vxPSWWWvyuy9vZYv1RubN8JsPTkxWeH1s6O9SjfaufRcIVno63v7QHSuN6U+OpPeNRTj0erJ+VfCapzjTac8XKMVZQ0ST1QLIe7qUooCT1JOMbyVJyUVLjOZ6+HNOq85lGRGnWu8+CMeRDQvXnPba5zzIp7BDoVGckh/Oe8xnJ+bznnOSdzyb5w0Wa5CVSJ6AQn97Cd0AgGMf7MD2JSaRlfVp1iU6MYbINgc7vBBAOWX6c9Gx06ox8fseEqnJ7lWeJKiJ5dWeJqbSVlNnsIm6sTN3wfPbw7AbfzxV8QTWaiWo06yqtlaHXyqDVMPRqGLQahl4Ng1bDAm4cd+O4G8e/4Dhep8XxMwXcYk/UTPbgWCqQHq/65yvilYVgvJvHhXQEOSSCNiZSfBC/bJVI/SSF+AEOghKpn7Xd/OEYHqlAWgwpfsQjlQgcmXW2OYcdAsE4ZnYqXnQLBG1MzvM98o8QgdQx5oBHYvnMBH1JjQSo3t4fVuTF1RMI2tvuK/QO45YE+rzRUxzPcOiyEQmMVtqggdFCyGQf2bDJRm4sc2PZVxTLaK0MvVZuaHNDmxvaPP+f0FZ/XqHNOZI5f61hBlOFRRz8JIJeRlfNsTDuRSBwjM2ZG+TrBIFUrnD6SqIwNlsYxe9PJYJeFhbZDH7fKRH04vg2kycyFd9iCATjcPymwPlNL0tbJd7D4xBIdyzvVjgsBPdl6Sj/D/6eQyJoY3SeRWZwGwKp9brDUjgqSwS9zAzyaRxzJVI13WbHcVJTG2lh5D9/I2r/rwDHUhud+hXlpxPnSTNy3xa6sdSNpW4sdWOpG0u/6ljaoMdSd4O6G9TdoF/SBu3+FxpAbzhIMQAA",
-    filter_url: "{{fl.地区}}{{fl.排序}}{{fl.剧情}}/id/{{fl.类型}}{{fl.语言}}{{fl.字母}}{{fl.年份}}",
-    filter_def: {
-        1: {
-            类型: "1"
-        },
-        2: {
-            类型: "2"
-        },
-        3: {
-            类型: "3"
-        },
-        4: {
-            类型: "4"
-        },
-        5: {
-            类型: "5"
-        }
+    title:'OmoFun',
+    模板:'mxpro',
+    host:'http://www.omofun.top',
+    // url:'/index.php/vod/show/id/fyclass/page/fypage.html',
+    url:'/index.php/vod/show/id/fyclassfyfilter.html',
+    filterable:1,//是否启用分类筛选,
+    filter_url:'{{fl.area}}{{fl.by}}{{fl.letter}}/page/fypage{{fl.year}}',
+    filter:{
+        "1":[{"key":"area","name":"地区","value":[{"n":"全部","v":""},{"n":"大陆","v":"/area/大陆"},{"n":"香港","v":"/area/香港"},{"n":"台湾","v":"/area/台湾"},{"n":"美国","v":"/area/美国"},{"n":"法国","v":"/area/法国"},{"n":"英国","v":"/area/英国"},{"n":"日本","v":"/area/日本"},{"n":"韩国","v":"/area/韩国"},{"n":"德国","v":"/area/德国"},{"n":"泰国","v":"/area/泰国"},{"n":"印度","v":"/area/印度"},{"n":"意大利","v":"/area/意大利"},{"n":"西班牙","v":"/area/西班牙"},{"n":"加拿大","v":"/area/加拿大"},{"n":"其他","v":"/area/其他"}]},{"key":"year","name":"年份","value":[{"n":"全部","v":""},{"n":"2023","v":"/year/2023"},{"n":"2022","v":"/year/2022"},{"n":"2021","v":"/year/2021"},{"n":"2020","v":"/year/2020"},{"n":"2019","v":"/year/2019"},{"n":"2018","v":"/year/2018"},{"n":"2017","v":"/year/2017"},{"n":"2016","v":"/year/2016"},{"n":"2015","v":"/year/2015"},{"n":"2014","v":"/year/2014"},{"n":"2013","v":"/year/2013"},{"n":"2012","v":"/year/2012"},{"n":"2011","v":"/year/2011"},{"n":"2010","v":"/year/2010"}]},{"key":"letter","name":"全部","value":[{"n":"全部","v":""},{"n":"A","v":"/letter/A"},{"n":"B","v":"/letter/B"},{"n":"C","v":"/letter/C"},{"n":"D","v":"/letter/D"},{"n":"E","v":"/letter/E"},{"n":"F","v":"/letter/F"},{"n":"G","v":"/letter/G"},{"n":"H","v":"/letter/H"},{"n":"I","v":"/letter/I"},{"n":"J","v":"/letter/J"},{"n":"K","v":"/letter/K"},{"n":"L","v":"/letter/L"},{"n":"M","v":"/letter/M"},{"n":"N","v":"/letter/N"},{"n":"O","v":"/letter/O"},{"n":"P","v":"/letter/P"},{"n":"Q","v":"/letter/Q"},{"n":"R","v":"/letter/R"},{"n":"S","v":"/letter/S"},{"n":"T","v":"/letter/T"},{"n":"U","v":"/letter/U"},{"n":"V","v":"/letter/V"},{"n":"W","v":"/letter/W"},{"n":"X","v":"/letter/X"},{"n":"Y","v":"/letter/Y"},{"n":"Z","v":"/letter/Z"},{"n":"0-9","v":"/letter/0-9"}]},{"key":"by","name":"排序","value":[{"n":"时间","v":"/by/time"},{"n":"人气","v":"/by/hits"},{"n":"评分","v":"/by/score"}]}]
     },
-    headers: {
-        "User-Agent": "MOBILE_UA"
-    },
-    timeout: 5000,
-    class_parse: "ul.navbar-items&&a;a&&Text;a&&href;(\\d+?)",
-    cate_exclude: "动漫资讯",
-    play_parse: true,
-    lazy: `js:        var html = JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);        var url = html.url;        if (html.encrypt == '1') {            url = unescape(url)        } else if (html.encrypt == '2') {            url = unescape(base64Decode(url))        }        if (/\\.m3u8|\\.mp4/.test(url)) {            input = {                jx: 0,                url: url,                parse: 0            }        } else {            input        }    `,
-    double: false,
-    推荐: 'body&&.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    一级: 'body&&.module-poster-item;a&&title;.lazyload&&data-original;.module-item-note&&Text;a&&href',
-    二级: {
-        "title": "h1&&Text",
-        "img": ".module-item-pic&&img&&data-original",
-        "desc": ".module-info-item:eq(4)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(1)&&Text;li.col-xs-12--span:eq(0)&&Text;;",
-        "content": ".show-desc&&Text",
-        "tabs": "body&&.module-tab-item",
-        "lists": "#panel1&&a",
-        tab_text: "body&&Text",
-        list_text: "body&&Text",
-        list_url: "a&&href"
-    },
-    searchUrl: '/vod/search/page/fypage/wd/**.html',
-    //detailUrl:'/vod/detail/id/fyid.html', //非必填,二级详情拼接链接
-    搜索: 'body&&.module-card-item;.module-card-item-title&&Text;.lazyload&&data-original;.module-item-note&&Text;a&&href',
+	filter_def:{
+		1:{by:'/by/time'}
+	},
+    searchUrl:'/index.php/vod/search/page/fypage/wd/**.html',
+    class_parse: '.navbar-items li:gt(1):lt(5);span&&Text;a&&href;/(\\d+).html',
 }
