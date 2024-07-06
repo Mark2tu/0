6c9b19de964c52acfc73385d808c435600ef9664
Mark2tu <33594111+Mark2tu@users.noreply.github.com>
2024-07-06 19:29:43 +0800

    Update OmoFun.js

--- js/OmoFun.js
+++ js/OmoFun.js
@@ -1,10 +1,10 @@
-muban.短视2.二级.img = '.detail-pic&&img&&data-original';
+muban.短视2.二级.img = '.module-item-pic&&img&&data-original';
 var rule = {
     title: 'OmoFun',
     模板:'短视2',
     host: 'https://omofun.life',
     homeUrl:'/label/new.html',
-	url: '/index.php/api/vod#type=fyclass&page=fypage',
+	url: '/vod/show/area/日本/id/4/page/fypage.html',
     class_name:'新旧番剧&剧场版',
     class_url:'2&1',
     detailUrl:'/vod/detail/id/fyid.html',
