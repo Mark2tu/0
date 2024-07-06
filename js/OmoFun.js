550edccbb7718a1603b668b44deed1d6ce2b3b3b
Mark2tu <33594111+Mark2tu@users.noreply.github.com>
2024-07-06 19:34:52 +0800

    Update OmoFun.js

--- js/OmoFun.js
+++ js/OmoFun.js
@@ -1,4 +1,3 @@
-muban.短视2.二级.img = '.module-item-pic&&img&&data-original';
 var rule = {
     title: 'OmoFun',
     模板:'短视2',
@@ -9,5 +8,17 @@ var rule = {
     class_url:'2&1',
     detailUrl:'/vod/detail/id/fyid.html',
     推荐:'.border-box .public-list-box;a&&title;.lazy&&data-original;.public-list-prb&&Text;a&&href',
+    二级: {
+        "title": "h1&&Text",
+        "img": ".module-item-pic&&img&&data-original",
+        "desc": ".module-info-item:eq(4)&&Text;.module-info-item:eq(2)&&Text;.module-info-item:eq(1)&&Text;li.col-xs-12--span:eq(0)&&Text;;",
+        "content": ".show-desc&&Text",
+        "tabs": "body&&.module-tab-item",
+        "lists": "#panel1&&a",
+        tab_text: "body&&Text",
+        list_text: "body&&Text",
+        list_url: "a&&href"
+    },
+    },
     double: false, // 推荐内容是否双层定位
 }
